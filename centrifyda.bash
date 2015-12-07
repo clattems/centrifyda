4a296c7cc4e6baef291f7ff8abc87bf6406a83a5
mark.clatterbuck <mark.clatterbuck@e-hps.com>
2015-12-07 12:37:52 -0600

    removed my email

--- centrifyda.bash
+++ centrifyda.bash
@@ -2,5 +2,5 @@
 hostname=$(/bin/hostname)
 dainfo -d | grep 'Connection refused' &> /dev/null
 if [ $? == 0 ]; then
-	  mail -s "Fix CentrifyDA on $hostname" mark.clatterbuck@e-hps.com < /dev/null
+	  mail -s "Fix CentrifyDA on $hostname" user@example.com < /dev/null
   fi
